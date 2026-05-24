e60ce63b7fd8b51847f1fb41c72a1c1da3d61225
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-05-24 16:15:47 +0300

    Connected DB, added middlewares & launched server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./config/db.js";
 
 const app = express();
-const port = 7339
+const port = process.env.PORT;
 
 // Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // DB
+connectDB();
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("✨ API's in its working era");
+});
+
+app.listen(port, () => {
+  console.log(
+    `✨ Ready for it? Server's live on port http://localhost:${port} ⚡`,
+  );
+});
