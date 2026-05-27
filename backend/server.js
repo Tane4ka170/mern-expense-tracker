5d53f1a04cdc30fe23c2f274ba376f4167a41648
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-05-27 16:19:41 +0300

    Registered userRouter for /api/auth endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -15,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Routes
+app.use("/api/auth", userRouter);
+
 app.get("/", (req, res) => {
   res.send("✨ API's in its working era");
 });
