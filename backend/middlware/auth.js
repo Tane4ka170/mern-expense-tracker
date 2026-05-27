6ea2a2bd5ab9a8a9199d0307564b98b5b6714cfe
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-05-27 16:20:51 +0300

    Fixed async/await bug & error handling in auth

--- backend/middlware/auth.js
+++ backend/middlware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export default function authMiddleware(req, res, next) {
+export default async function authMiddleware(req, res, next) {
   // Grab the token
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer")) {
@@ -15,11 +15,16 @@ export default function authMiddleware(req, res, next) {
   // To verify the token
   try {
     const payload = jwt.verify(token, JWT_SECRET);
-      const user = await User.findById(payload.id).select("-password");
-      if (!user) {
-        return res.status(401).json({success:false, message: "User not found" });
-      }
-      req.user = user;
-      next();
-  } catch (error) {}
+    const user = await User.findById(payload._id).select("-password");
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("JWT verification failed:", error);
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
