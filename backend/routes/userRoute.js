25d8661b7d3bc73aef008f7c2a73e3da34c6b439
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-05-28 17:26:50 +0300

    Added protected profile routes to userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,4 +1,12 @@
 import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+  updateProfile,
+  updatePassword,
+} from "../controllers/userController.js";
+import authMiddleware from "../middlware/auth.js";
 
 const userRouter = express.Router();
 
@@ -6,3 +14,8 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 
 // Protected routes
+userRouter.get("/me", authMiddleware, getCurrentUser);
+userRouter.put("/profile", authMiddleware, updateProfile);
+userRouter.put("/password", authMiddleware, updatePassword);
+
+export default userRouter;
