b90c833072904c03b9b849f1e4c5eb0f8e5309aa
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-05-30 17:19:26 +0300

    Finished CRUD logic for income endpoints

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -10,6 +10,77 @@ export async function addIncome(req, res) {
       return res.status(400).json({ message: "Please fill in all fields" });
     }
 
-    const newIncome = new incomeModel({});
+    const newIncome = new incomeModel({
+      userId,
+      description,
+      amount,
+      category,
+      date: new Date(date),
+    });
+    await newIncome.save();
+    res.json({
+      success: true,
+      message: "Income added successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
+
+// to get income(all)
+export async function getIncome(req, res) {
+  const userId = req.user._id;
+
+  try {
+    const income = await incomeModel.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
+
+//Update an income
+export async function updateIncome(req, res) {
+  const { id } = req.params;
+  const userId = req.user._id;
+  const { description, amount } = req.body;
+
+  try {
+    const updateIncome = await incomeModel.findOneAndUpdate(
+      {
+        _id: id,
+        userId,
+      },
+      { description, amount },
+      { new: true },
+    );
+    if (!updateIncome) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+
+    res.json({
+      success: true,
+      message: "Income updated successfully",
+      data: updateIncome,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
+
+//Delete an income
+export async function deleteIncome(req, res) {
+  try {
+    const income = await incomeModel.findByIdAndDelete(_id: req.params.id);
+    if (!income) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+    res.json({
+      success: true,
+      message: "Income deleted successfully",
+    })
   } catch (error) {}
 }
