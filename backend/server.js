6df270d04aed912f97c15b3454c0d24893b33bc1
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-06-07 12:00:00 +0300

    Something did

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
 import incomeRouter from "./routes/incomeRoute.js";
+import expenseRouter from "./routes/expenseRoute.js";
+import dashboardRouter from "./routes/dashboardRoute.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -19,6 +21,8 @@ connectDB();
 // Routes
 app.use("/api/auth", userRouter);
 app.use("/api/income", incomeRouter);
+app.use("/api/expense", expenseRouter);
+app.use("/api/dashboard", dashboardRouter);
 
 app.get("/", (req, res) => {
   res.send("✨ API's in its working era");
