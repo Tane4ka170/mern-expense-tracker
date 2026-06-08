7b008cbf325a18dcc4a1358c18310b3146da6bfd
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-06-08 16:21:05 +0300

    Setup auth state & logout handling logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
+
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+  const clearAuth = () => {
+    try {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      sessionStorage.removeItem("token");
+      sessionStorage.removeItem("user");
+    } catch (error) {
+      console.log("ClearAuth Error: ", error);
+    }
+    setUser(null);
+    setToken(null);
+  };
+
+  const handleLogout = () => {
+    clearAuth();
+    navigate("/login");
+  };
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Routes>
+        <Route element={<Layout />} />
+        <Route path="/*" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 };
 
