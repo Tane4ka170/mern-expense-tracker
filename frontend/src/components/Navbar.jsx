2e410703d37c79451444da3d68d4bab8151e6fba
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-06-09 17:34:21 +0300

    Implemented Navbar navigation & state logic

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import { navbarStyles } from "../assets/dummyStyles";
-import 'img1' from '../assets/logo.png';
+import img1 from "../assets/logo.png";
+import { useRef, useState } from "react";
 
-const Navbar = ({user:propUser, onLogout}) => {
-    return <header className={navbarStyles.header}>
-        <div className={navbarStyles.container}></div>
-  </header>;
+const Navbar = ({ user: propUser, onLogout }) => {
+  const navigate = useNavigate();
+  const menuRef = useRef();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const user = propUser || {
+    name: "",
+    email: "",
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+  return (
+    <header className={navbarStyles.header}>
+      <div className={navbarStyles.container}>
+        {/* Logo */}
+        <div
+          className={navbarStyles.logoContainer}
+          onClick={() => navigate("/")}
+        >
+          <div className={navbarStyles.logoImage}>
+            <img src={img1} alt="logo" />
+          </div>
+          <span className={navbarStyles.logoText}>Expense Tracker</span>
+        </div>
+
+        {/* If the user is present*/}
+        {user && (
+          <div className={navbarStyles.userContainer} ref={menuRef}>
+            <button onClick={toggleMenu} className={navbarStyles.userButton}>
+              <div className="relative">
+                <div className={navbarStyles.userAvatar}>
+                  {user?.name?.[0]?.toUpperCase() || "U"}
+                </div>
+                <div className={navbarStyles.statusIndicator}></div>
+              </div>
+
+              <div className={navbarStyles.userTextContainer}>
+                <p className={navbarStyles.userName}>{user?.name || "User"}</p>
+              </div>
+            </button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
