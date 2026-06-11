8b675a6a69e04560d4e98037a740400826a15ac0
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2026-06-11 15:59:40 +0300

    Created basic Sidebar component structure

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import { sidebarStyles, cn } from "../assets/dummyStyles";
+
+const Sidebar = ({ user, isCollapsed, setIsCollapsed }) => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
